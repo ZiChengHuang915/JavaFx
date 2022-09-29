8c8c8953b2025caaff6807e93ab6c16b8efc9dcd
ZI-DESKTOP <zc3huang@uwaterloo.ca>
2022-09-29 15:06:47 -0400

    added toolbar and status bar label

--- a1/a1-basic/src/main/kotlin/Main.kt
+++ a1/a1-basic/src/main/kotlin/Main.kt
@@ -1,14 +1,45 @@
 import javafx.application.Application
+import javafx.collections.FXCollections
+import javafx.geometry.Pos
 import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.CheckBox
+import javafx.scene.control.ChoiceBox
 import javafx.scene.control.Label
-import javafx.scene.layout.GridPane
+import javafx.scene.layout.*
 import javafx.stage.Stage
+
 
 class Main : Application()  {
     override fun start(stage: Stage) {
-        val root = GridPane()
+        val root = BorderPane()
         val statusBar = Label("temp label")
-        root.add(statusBar, 0, 0)
+
+        val viewLabel = Label("View: ")
+        val viewListButton = Button("List")
+        val viewGridButton = Button("Grid")
+        val viewGroup = HBox(viewLabel, viewListButton, viewGridButton)
+
+        val archivedLabel = Label("Show archived: ")
+        val archivedCheckBox = CheckBox()
+        val archivedGroup = HBox(archivedLabel, archivedCheckBox)
+
+        val orderLabel = Label("Order by: ")
+        val orderDropDownOptions = FXCollections.observableArrayList("Length (asc)", "Length (desc)")
+        val orderDropDown = ChoiceBox(orderDropDownOptions)
+        val orderGroup = HBox(orderLabel, orderDropDown)
+
+        val region = Region()
+        HBox.setHgrow(region, Priority.ALWAYS)
+
+        val clearButton = Button("Clear")
+
+        val toolBar = HBox(viewGroup, archivedGroup, orderGroup, region, clearButton)
+
+        BorderPane.setAlignment(toolBar, Pos.TOP_CENTER)
+        BorderPane.setAlignment(statusBar, Pos.BOTTOM_CENTER)
+        root.top = toolBar
+        root.bottom = statusBar
 
         stage.title = "CS349 - A1 Notes - zc3huang"
         stage.scene = Scene(root, 800.0, 600.0)
@@ -17,6 +48,5 @@ class Main : Application()  {
             minWidth = 640.0
             minHeight = 480.0
         }
-
     }
 }
