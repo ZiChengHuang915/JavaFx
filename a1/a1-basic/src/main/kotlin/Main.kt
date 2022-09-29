eb4f90a68903875f6a1c3fd4fd1ff530b35b1abf
ZI-DESKTOP <zc3huang@uwaterloo.ca>
2022-09-29 14:04:43 -0400

    1

--- a1/a1-basic/src/main/kotlin/Main.kt
+++ a1/a1-basic/src/main/kotlin/Main.kt
@@ -1,8 +1,22 @@
 import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.Label
+import javafx.scene.layout.GridPane
 import javafx.stage.Stage
 
 class Main : Application()  {
-    override fun start(primaryStage: Stage) {
-        primaryStage.show()
+    override fun start(stage: Stage) {
+        val root = GridPane()
+        val statusBar = Label("temp label")
+        root.add(statusBar, 0, 0)
+
+        stage.title = "CS349 - A1 Notes - zc3huang"
+        stage.scene = Scene(root, 800.0, 600.0)
+        stage.show()
+        stage.apply {
+            minWidth = 640.0
+            minHeight = 480.0
+        }
+
     }
 }
